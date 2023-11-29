0e51343498e436592c62b80310bd58b543cf61e9
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2023-11-29 15:07:30 +0530

    Update index.js

--- Snapshots/server/index.js
+++ Snapshots/server/index.js
@@ -17,13 +17,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors(
-  {
-    origin: ["https://deploy-mern-1whq.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }  
-));
+app.use(cors());
 
 app.use("/", postRoutes);
 app.use("/user", userRoutes);

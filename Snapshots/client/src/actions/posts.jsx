60255335e76877870ac7f8d8ec2946915e993883
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2023-12-03 13:05:19 +0530

    Add files via upload

---
+++ Snapshots/client/src/actions/posts.jsx
@@ -0,0 +1,79 @@
+// we are using redux thunk for asynchronous actions
+
+import * as api from "../api/index.js";
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes.js";
+
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+// export const getPostsBySearch = (searchQuery) => async (dispatch) => {
+//   try {
+//     const {
+//       data: { data },
+//     } = await api.fetchPostsBySearch(searchQuery);
+//     console.log("Action getPostsBySearch data", data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+export const getPostsBySearch = (searchQuery, searchBy) => async (dispatch) => {
+  try {
+    const {
+      data: { data },
+    } = await api.fetchPostsBySearch(searchQuery, searchBy);
+    console.log("Action getPostsBySearch data", data);
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+export const createPost = (newPost) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(newPost);
+    dispatch({ type: CREATE, payload: data });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+  try {
+    const { data } = await api.likePost(id, user?.token);
+    dispatch({ type: LIKE, payload: data });
+  } catch (error) {
+    console.error(error.message);
+  }
+};

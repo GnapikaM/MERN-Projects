2d1b46013fae554a909e720c83762d1b69aeb2d6
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2023-12-03 20:15:33 +0530

    Update NewPost.jsx

--- Snapshots/client/src/components/NavLinks/NewPost/NewPost.jsx
+++ Snapshots/client/src/components/NavLinks/NewPost/NewPost.jsx
@@ -103,143 +103,146 @@ const NewPost = ({ currentId, setCurrentId }) => {
   };
 
   return (
-    <div className="relative flex flex-col items-center justify-center my-20">
+    <div className="bg-bg2 px-4 pt-20 bg-no-repeat bg-cover flex flex-col items-center justify-center h-[120vh]">
       {notification && (
-        <div className="bg-green-500 text-white py-2 px-4 text-xl font-semibold font-mono rounded shadow-md absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="bg-[#00ADB5] text-white py-2 px-4 text-xl font-semibold rounded shadow-md absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           {notificationMessage}
         </div>
       )}
 
-      <form
-        className="flex flex-col justify-center bg-[#393E46] text-white rounded-xl p-5"
-        onSubmit={handleSubmitButton}
-      >
-        <div>
+      <form onSubmit={handleSubmitButton}>
+        <div className="grid">
           <h1 className="text-center font-bold text-3xl mb-6 font-palanquin">
             {currentId ? "Editing a " : "Add new "} Post
           </h1>
         </div>
-        <label htmlFor="category" className="relative flex items-center">
-          <p className="w-24 px-2 py-3 font-semibold absolute left-[1px] text-white rounded bg-[#393E46] text-lg text-right">
-            Category:
-          </p>
+        <div className="flex mb-2 items-center max-sm:flex-col max-sm:items-start">
+          <div className="w-1/4 mr-2 font-semibold text-base max-sm:w-full text-white">
+            <label htmlFor="">Category: </label>
+          </div>
           <input
             type="text"
-            name="category"
+            name="firstName"
             placeholder="Select"
-            // value={postData.category}
-            value={postData.category = category}
+            value={(postData.category = category)}
             onChange={(e) => handleInputChange(e, "category")}
-            className="w-full outline-none my-1 p-3 text-black rounded pl-[105px]"
+            className="outline-none p-2 w-3/4 max-sm:w-full my-1"
             required
             autoFocus
           />
-        </label>
-        <div className="flex justify-around my-2">
+        </div>
+        <div className="flex justify-around my-2 text-white">
           {categoryOptions.map((category) => (
-            <p key={category.option} className="border-[1px] rounded-full px-2 py-1 cursor-pointer" onClick={() => setCategory(category.option)}>{category.option}</p>
+            <p
+              key={category.option}
+              className="border-[1px] rounded-full px-2 py-1 cursor-pointer"
+              onClick={() => setCategory(category.option)}
+            >
+              {category.option}
+            </p>
           ))}
         </div>
-        <label htmlFor="category" className="relative flex items-center">
-          <p className="w-24 px-2 py-3 font-semibold absolute left-[1px] text-white rounded bg-[#393E46] text-lg text-right">
-            Place:
-          </p>
+        <div className="flex mb-2 items-center max-sm:flex-col max-sm:items-start text-white">
+          <div className="w-1/4 mr-2 font-semibold text-base max-sm:w-full">
+            <label htmlFor="">Place: </label>
+          </div>
           <input
             type="text"
             name="place"
             placeholder="E.g., India"
             value={postData.place}
             onChange={(e) => handleInputChange(e, "place")}
-            className="w-full outline-none my-1 p-3 text-black rounded pl-[105px]"
+            className="outline-none p-2 w-3/4 max-sm:w-full my-1"
             required
           />
-        </label>
-        <label htmlFor="title" className="relative flex items-center">
-          <p className="w-24 px-2 py-3 font-semibold absolute left-[1px] text-white rounded bg-[#393E46] text-lg text-right">
-            Title:
-          </p>
+        </div>
+        <div className="flex mb-2 items-center max-sm:flex-col max-sm:items-start">
+          <div className="w-1/4 mr-2 font-semibold text-base max-sm:w-full text-white">
+            <label htmlFor="">Title: </label>
+          </div>
           <input
             type="text"
             name="title"
             placeholder="E.g., Sri Venkateshware Temple"
             value={postData.title}
             onChange={(e) => handleInputChange(e, "title")}
-            className="w-full outline-none my-1 p-3 text-black rounded pl-[105px]"
+            className="outline-none p-2 w-3/4 max-sm:w-full my-1"
             required
           />
-        </label>
-        <label htmlFor="message" className="relative flex items-center">
-          <p className="w-24 px-2 py-6 font-semibold absolute left-[1px] text-white rounded bg-[#393E46] text-lg text-right">
-            Message:
-          </p>
+        </div>
+        <div className="flex mb-2 items-center max-sm:flex-col max-sm:items-start">
+          <div className="w-1/4 mr-2 font-semibold text-base max-sm:w-full text-white">
+            <label htmlFor="">Message: </label>
+          </div>
           <textarea
+            type="text"
             name="message"
             id="message"
-            type="text"
             placeholder="Type Someting..."
             rows="2"
             value={postData.message}
             onChange={(e) => handleInputChange(e, "message")}
-            className="w-full outline-none my-1 p-3 text-black rounded pl-[105px]"
+            className="outline-none p-2 w-3/4 max-sm:w-full my-1"
             required
           />
-        </label>
-        <label htmlFor="tags" className="relative flex items-center">
-          <p className="w-24 px-2 py-3 font-semibold absolute left-[1px] text-white rounded bg-[#393E46] text-lg text-right">
-            Tags:
-          </p>
+        </div>
+        <div className="flex mb-2 items-center max-sm:flex-col max-sm:items-start">
+          <div className="w-1/4 mr-2 font-semibold text-base max-sm:w-full text-white">
+            <label htmlFor="">Tags: </label>
+          </div>
           <input
             type="text"
             name="tags"
             placeholder="E.g., Travel,Tourism"
             value={postData.tags}
             onChange={(e) => handleTagInputChange(e, "tags")}
-            className="w-full outline-none my-1 p-3 text-black rounded pl-[105px]"
+            className="outline-none p-2 w-3/4 max-sm:w-full my-1"
             required
           />
-        </label>
+        </div>
         <div>
           {!postData.selectedFile ? (
-            <label htmlFor="selectedFile" className="flex items-center">
-              <p className="w-24 font-semibold text-right">Post Image:</p>
+            <div className="flex mb-2 items-center max-sm:flex-col max-sm:items-start">
+              <div className="w-1/4 mr-2 font-semibold text-base max-sm:w-full text-white">
+                <label htmlFor="">Post Image: </label>
+              </div>
               <input
                 type="file"
                 name="selectedFile"
-                className="outline-none border-none my-2 ml-2 w-24"
                 onChange={handleFileChange}
+                className="outline-none p-2 w-3/4 max-sm:w-full my-1"
                 required
               />
-            </label>
+            </div>
           ) : (
             <div className="flex items-center space-x-2 mt-4">
-              <div className="flex items-center">
-                <p className="w-24 font-semibold">Post Image:</p>
-                <div className="flex flex-col">
-                  <img
-                    src={postData.selectedFile}
-                    alt="Preview"
-                    className="h-16 w-16 object-cover rounded-md my-2"
-                  />
-                  <button
-                    className="bg-white text-[#393E46] p-1 rounded hover:scale-105 hover:transition-all font-medium"
-                    onClick={() => handleClearSelection("selectedFile")}
-                  >
-                    Clear Selection
-                  </button>
-                </div>
+              <div className="flex items-center mr-2 font-semibold text-base max-sm:w-full">
+                <label htmlFor="">Post Image: </label>
+                <img
+                  src={postData.selectedFile}
+                  alt="preview"
+                  className="h-16 w-16 object-cover rounded-md m-2"
+                />
+                <button
+                  className="ml-2 bg-white text-[#393E46] p-1 rounded hover:scale-105 hover:transition-all font-medium"
+                  onClick={() => handleClearSelection("selectedFile")}
+                >
+                  Clear Selection
+                </button>
               </div>
             </div>
           )}
         </div>
+
         <button
           type="submit"
-          className="bg-white text-[#393E46] p-1 mt-4 mb-2 font-semibold uppercase rounded"
+          className="bg-white text-[#393E46] w-full p-1 mt-4 mb-2 font-semibold uppercase rounded"
         >
           Submit
         </button>
         <button
           type="button"
-          className="bg-white text-[#393E46] p-1 font-semibold uppercase rounded"
+          className="bg-white text-[#393E46] w-full p-1 font-semibold uppercase rounded"
           onClick={handleButtonClear}
         >
           Clear

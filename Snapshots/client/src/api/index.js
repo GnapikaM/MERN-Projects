382053507795074a3dcd76138246d3452448bfdb
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2023-12-03 13:08:43 +0530

    Update index.js

--- Snapshots/client/src/api/index.js
+++ Snapshots/client/src/api/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// const API = axios.create({ baseURL: "http://localhost:3000" });
 const API = axios.create({ baseURL: "https://snapshots.onrender.com/" });
 
 API.interceptors.request.use((req) => {
@@ -11,7 +10,6 @@ API.interceptors.request.use((req) => {
 })
 
 export const fetchPosts = () => API.get('/');
-// export const fetchPostsBySearch = (searchQuery) => API.get(`/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
 export const fetchPostsBySearch = (searchQuery, searchBy) => API.get(`/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}&searchBy=${searchBy}`);
 export const createPost = (newPost) => API.post('/', newPost);
 export const updatePost = (id, updatedPost) => API.patch(`/${id}`, updatedPost);

6c61c507e470c1dc377b7fc2810604ae771568f8
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2024-04-21 21:39:35 +0530

    Delete E-Commerce (ComfyCloth) directory

--- E-Commerce (ComfyCloth)/server/routes/ProductRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { fetchAllProducts } from "../controller/productController.js";
-
-const router = express.Router();
-
-router.route("/").get(fetchAllProducts);
-
-export default router;

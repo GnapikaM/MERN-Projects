388ab8726bdb7b644c36cb851d39eb844ab10605
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2024-04-29 09:29:09 +0530

    Update ProductModel.js

--- ComfyClothECommerce/server/model/ProductModel.js
+++ ComfyClothECommerce/server/model/ProductModel.js
@@ -1,21 +1,5 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  id: Number,
-  category: String,
-  company: String,
-  name: String,
-  color: [String],
-  price: Number,
-  origPrice: Number,
-  discount: Number,
-  rating: Number,
-  reviews: String,
-  quantity: Number,
-  productType: [String],
-  img1: String,
-  img2: String,
-  img3: String,
-});
+const productSchema = new mongoose.Schema();
 
 export const Product = mongoose.model("Product", productSchema);

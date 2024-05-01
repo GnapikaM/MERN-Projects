aa4bed53e7d9577909660306fe7717644fec2367
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2024-05-01 13:22:29 +0530

    Update userController.js

--- ComfyClothECommerce/server/controller/userController.js
+++ ComfyClothECommerce/server/controller/userController.js
@@ -56,3 +56,14 @@ export const login = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const fetchUsers = async (req, res) => {
+  try {
+    // Fetch all users from the database
+    const users = await UserModel.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};

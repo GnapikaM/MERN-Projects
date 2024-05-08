460507095e121408aea87fda863862336a4a9d8d
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2024-05-08 09:36:33 +0530

    Update userController.js

--- ComfyClothECommerce/server/controller/userController.js
+++ ComfyClothECommerce/server/controller/userController.js
@@ -69,3 +69,24 @@ export const fetchUsers = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const deleteAccount = async (req, res) => {
+  try {
+    const {userId} = req.params;
+
+    console.log("User Id: ", userId);
+
+    if(!userId) {
+      return res.status(400).json({ message: 'User ID is required.' });
+    }
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+    await UserModel.findByIdAndDelete(userId);
+    res.status(200).json({ message: 'User deleted successfully.' });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).json({ message: 'An error occurred while deleting user.' });
+  }
+}

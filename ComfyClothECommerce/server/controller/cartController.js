985293d916fb652927654952961bea9ef7a7bd22
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2024-05-11 17:36:51 +0530

    Update cartController.js

--- ComfyClothECommerce/server/controller/cartController.js
+++ ComfyClothECommerce/server/controller/cartController.js
@@ -15,31 +15,10 @@ export const fetchCartItems = async (req, res) => {
   }
 };
 
-export const addToCart = async (req, res) => {
-  const { userId } = req.params;
-  const { productId } = req.body;
-
-  try {
-    const user = await UserModel.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    if (user.cart.includes(productId)) {
-      return res.status(400).json({ message: "Product already in cart" });
-    }
-    user.cart.push(productId);
-    await user.save();
-    res.status(200).json({ message: "Product added to cart successfully" });
-  } catch (error) {
-    console.error("Error adding product to cart: ", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
-
 export const removeFromCart = async (req, res) => {
   const { userId, productId } = req.params;
   try {
-    const user = await UserModel.findOneAndUpdate(
+    const user = await UserModel.findByIdAndDelete(
       { _id: userId },
       { $pull: { cart: productId } },
       { new: true }
@@ -52,6 +31,72 @@ export const removeFromCart = async (req, res) => {
     res.status(200).json({ message: "Product removed from cart successfully" });
   } catch (error) {
     console.error("Error removing product from cart: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const addToCart = async (req, res) => {
+  const { userId } = req.params;
+  const { product, quantity, size } = req.body;
+
+  try {
+    const productId = product?._id;
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const existingProductIndex = user.cart.findIndex(
+      (item) => String(item.product) === productId
+    );
+    if (existingProductIndex !== -1) {
+      // If the product exists, update the quantity and size
+      user.cart[existingProductIndex].quantity += quantity;
+      user.cart[existingProductIndex].size = size;
+    } else {
+      // If the product doesn't exist, add it to the cart
+      user.cart.push({
+        product: productId,
+        image: product?.images[0],
+        name: product?.name,
+        company: product?.company,
+        price: product?.price,
+        origPrice: product?.origPrice,
+        discount: product?.discount,
+        quantity,
+        size,
+      });
+    }
+    await user.save();
+    res.status(200).json({ message: "Product added to cart successfully" });
+  } catch (error) {
+    console.error("Error adding product to cart: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const updateCartItem = async (req, res) => {
+  const { userId } = req.params;
+  const { productId, quantity, size } = req.body;
+
+  try {
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const index = user.cart.findIndex(
+      (item) => String(item._id) === productId
+    );
+    if (index !== -1) {
+      user.cart[index].quantity = quantity;
+
+      user.cart[index].size = size;
+      await user.save();
+      res.status(200).json({ message: "Cart item updated successfully" });
+    } else {
+      res.status(404).json({ message: "Cart item not found" });
+    }
+  } catch (error) {
+    console.error("Error updating cart item: ", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };

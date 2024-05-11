03917a3221d69308326d3d88e73a0c0d49eb633a
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2024-05-11 17:42:03 +0530

    Update CartRoutes.js

--- ComfyClothECommerce/server/routes/CartRoutes.js
+++ ComfyClothECommerce/server/routes/CartRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { fetchCartItems, addToCart, removeFromCart, removeAllFromCart } from "../controller/cartController.js";
+import { fetchCartItems, addToCart, removeFromCart, removeAllFromCart, updateCartItem } from "../controller/cartController.js";
 
 const router = express.Router();
 
 router.route("/:userId/cart").get(fetchCartItems);
 router.route("/:userId/cart/add").post(addToCart);
+router.route("/:userId/cart/update").patch(updateCartItem)
 router.route("/:userId/cart/:productId").delete(removeFromCart);
 router.route("/:userId/cart/remove-all").delete(removeAllFromCart);
 

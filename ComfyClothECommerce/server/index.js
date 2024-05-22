69fd19afe81e9f972e43b1eb8441182498027cc9
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2024-05-22 20:57:26 +0530

    Update index.js

--- ComfyClothECommerce/server/index.js
+++ ComfyClothECommerce/server/index.js
@@ -1,131 +1,9 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import path from "path";
-
-// import connectDB from "./database/connectDB.js";
-// import userRoutes from "./routes/UserRoutes.js";
-// import wishlistRoutes from "./routes/WishlistRoutes.js";
-// import ProductRoutes from "./routes/ProductRoutes.js";
-// import ContactRoutes from "./routes/ContactRoutes.js";
-// import cartRoutes from "./routes/CartRoutes.js";
-// import AddressRoutes from "./routes/AddressRoutes.js"
-// import OrderRoutes from "./routes/OrderRoutes.js";
-// import ReviewRoutes from "./routes/ReviewRoutes.js";
-
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.use(bodyParser.json({ limit: "50mb", extended: true }));
-// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-// app.use(cors());
-
-// app.use("/user", userRoutes);
-// app.use("/user", wishlistRoutes);
-// app.use("/user", cartRoutes);
-// app.use("/", ProductRoutes);
-// app.use("/contact", ContactRoutes);
-// app.use("/addresses", AddressRoutes);
-// app.use("/", OrderRoutes);
-// app.use("/products", ReviewRoutes);
-
-// const __filename = new URL(import.meta.url).pathname;
-// const __dirname = path.dirname(__filename);
-// console.log(__dirname);
-// app.use(express.static(path.join(__dirname, "../client/dist")));
-
-// app.get("*", function (req, res) {
-//   const indexPath = path.resolve(__dirname, "../client/dist/index.html");
-//   res.sendFile(indexPath);
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hello");
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port: http://localhost:${PORT}`);
-// });
-
-// import express from "express";
-// import bodyParser from "body-parser";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import path from "path";
-// import { Server } from "socket.io";
-// import http from "http";
-
-// import connectDB from "./database/connectDB.js";
-// import userRoutes from "./routes/UserRoutes.js";
-// import wishlistRoutes from "./routes/WishlistRoutes.js";
-// import ProductRoutes from "./routes/ProductRoutes.js";
-// import ContactRoutes from "./routes/ContactRoutes.js";
-// import cartRoutes from "./routes/CartRoutes.js";
-// import AddressRoutes from "./routes/AddressRoutes.js"
-// import OrderRoutes from "./routes/OrderRoutes.js";
-// import ReviewRoutes from "./routes/ReviewRoutes.js";
-
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
-// const PORT = process.env.PORT || 3000;
-
-// app.use(bodyParser.json({ limit: "50mb", extended: true }));
-// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-// app.use(cors());
-
-// app.use("/user", userRoutes);
-// app.use("/user", wishlistRoutes);
-// app.use("/user", cartRoutes);
-// app.use("/", ProductRoutes);
-// app.use("/contact", ContactRoutes);
-// app.use("/addresses", AddressRoutes);
-// app.use("/", OrderRoutes);
-// app.use("/products", ReviewRoutes);
-
-// const __filename = new URL(import.meta.url).pathname;
-// const __dirname = path.dirname(__filename);
-// app.use(express.static(path.join(__dirname, "../client/dist")));
-
-// app.get("*", function (req, res) {
-//   const indexPath = path.resolve(__dirname, "../client/dist/index.html");
-//   console.log("Index path: ", indexPath);
-//   res.sendFile(indexPath);
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hello");
-// });
-
-// io.on("connection", (socket) => {
-//   console.log("A client connected");
-
-//   ReviewModel.watch().on("change", (change) => {
-//     if (change.operationType === "update") {
-//       const updatedReview = change.fullDocument;
-//       const { likeCount, dislikeCount } = updatedReview;
-//       io.emit("reviewCountUpdated", { likeCount, dislikeCount });
-//     }
-//   });
-// })
-
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port: http://localhost:${PORT}`);
-// });
-
-
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
+import Stripe from "stripe";
 
 import connectDB from "./database/connectDB.js";
 import userRoutes from "./routes/UserRoutes.js";
@@ -133,7 +11,7 @@ import wishlistRoutes from "./routes/WishlistRoutes.js";
 import ProductRoutes from "./routes/ProductRoutes.js";
 import ContactRoutes from "./routes/ContactRoutes.js";
 import cartRoutes from "./routes/CartRoutes.js";
-import AddressRoutes from "./routes/AddressRoutes.js"
+import AddressRoutes from "./routes/AddressRoutes.js";
 import OrderRoutes from "./routes/OrderRoutes.js";
 import ReviewRoutes from "./routes/ReviewRoutes.js";
 
@@ -142,6 +20,7 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const stripe = new Stripe(process.env.STRIPE_SECRET);
 
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -158,16 +37,39 @@ app.use("/addresses", AddressRoutes);
 app.use("/", OrderRoutes);
 app.use("/products", ReviewRoutes);
 
-// const __dirname = path.resolve();
-// // app.use(express.static(path.join(__dirname, "client", "dist")));
-// app.use(express.static(path.join(__dirname, "../client/dist")));
-// console.log("Path name: ", __dirname);
+app.post("/create-payment-intent", async (req, res) => {
+  const { amount, description, customerAddress } = req.body;
+  console.log("create paymenet intent: ", customerAddress);
 
-// app.get("*", function (req, res) {
-//   const indexPath = path.resolve(__dirname, "client", "dist", "index.html");
-//   console.log("Index path: ", indexPath);
-//   res.sendFile(indexPath);
-// });
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+      description: description || "Payment description",
+      shipping: {
+        name: customerAddress.name,
+        address: {
+          line1: customerAddress.street,
+          city: customerAddress.city,
+          state: customerAddress.state,
+          postal_code: customerAddress.zip,
+          country: "US", // Specify the country
+        },
+        phone: customerAddress.phone,
+      },
+    });
+
+    console.log("Payment Intent created:", paymentIntent);
+
+    res.status(200).send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+    });
+  }
+});
 
 const __dirname = path.resolve();
 const parentDir = path.dirname(path.dirname(__dirname));
@@ -177,7 +79,6 @@ app.get("*", function (req, res) {
   const indexPath = path.resolve(__dirname, "../client/dist/index.html");
   res.sendFile(indexPath);
 });
-
 
 app.get("/", (req, res) => {
   res.send("Hello");
